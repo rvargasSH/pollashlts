f42c6e82e8970f6bb91fbdf081227a0b7e817811
Reynaldo Vargas Gaitan <vargas.reynaldo@sthonore.com.co>
2022-11-03 16:01:16 -0500

    changes to set asset instead mix on call css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,10 +55,10 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ mix('resources/js/app.js') }}"></script>
-    <script src="{{ mix('resources/js/vue.min.js') }}"></script>
+    <script src="{{ asset('resources/js/app.js') }}"></script>
+    <script src="{{ asset('resources/js/vue.min.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script src="{{ mix('resources/js/toastr.min.js') }}"></script>
+    <script src="{{ asset('resources/js/toastr.min.js') }}"></script>
 </body>
 </html>
